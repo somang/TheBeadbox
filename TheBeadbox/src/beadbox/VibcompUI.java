ca218cc1607dfa05bbd5ad7392ced08c449892a4
somang <sixjuwel@gmail.com>
2015-06-03 14:35:36 -0400

    look and feel in windows

--- TheBeadbox/src/beadbox/VibcompUI.java
+++ TheBeadbox/src/beadbox/VibcompUI.java
@@ -663,13 +663,21 @@ public class VibcompUI extends javax.swing.JFrame {
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
+            /*
+             Metal
+             Nimbus
+             CDE/Motif
+             Windows
+             Windows Classic
+             */
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                System.out.println(info.getName());
+                if("Windows".equals(info.getName())){
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
+        }catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(VibcompUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
             java.util.logging.Logger.getLogger(VibcompUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
